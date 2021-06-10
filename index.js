48e5696a76768d74a0a166eb12b24bd97f7dfd55
MCA-99 <capdet2@gmail.com>
2021-06-10 21:49:24 +0200

    a

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 // Wait response from popup and actuate over page
 
 chrome.runtime.onMessage.addListener(({ input_value }) => {
-    function doSearch(text, backgroundColor) {
-        if (window.find && window.getSelection) {
-            document.designMode = "on";
-            var sel = window.getSelection();
-            sel.collapse(document.body, 0);
-    
-            while (window.find(text)) {
-                document.execCommand("HiliteColor", false, backgroundColor);
-                sel.collapseToEnd();
-            }
-            document.designMode = "off";
-        }
+    if (document.getElementsByClassName("selectedText highlighted").length > 0){
+        unhighlight();
     }
-
+    
     doSearch(input_value, "yellow");
 });
+
+function doSearch(text, backgroundColor) {
+    if (window.find && window.getSelection) {
+        document.designMode = "on";
+        var sel = window.getSelection();
+        sel.collapse(document.body, 0);
+        var y_scroll = window.scrollY;
+
+        while (window.find(text)) {
+            document.execCommand("HiliteColor", false, backgroundColor);
+            sel.collapseToEnd();
+        }
+        document.designMode = "off";
+        window.scrollTo(0, y_scroll);
+    }
+}
+
+function unhighlight(){
+    document.execCommand("undo"); 
+}
